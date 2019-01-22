78f0fa4c5376089ce026edb455d36158bfb80511
casper52 <casper_52@naver.com>
2019-01-22 11:21:02 +0900

    react example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyHello from './ex01/MyHello'
+import NameTag from './ex01/NameTag'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <MyHello></MyHello>
+            <NameTag name='Captain' age='80'></NameTag>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React First app
+            </a>
+          </header>
+        </div>
     );
   }
 }
